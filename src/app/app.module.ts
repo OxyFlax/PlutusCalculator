a848647f206071796ea2679ebf91def0fdb0478b
Gilles Thewissen <gilles.thewissen@gmail.com>
2020-07-15 22:14:58 +0200

    Start of navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+import { ClickOutsideDirective } from './Directives/click-outside.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ClickOutsideDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+  showGamesDropDown: boolean = false;
+  showSettingsDropDown: boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  showDropDown(event, type) {
+    if (type == "games"){
+      if(!this.showGamesDropDown){
+        this.showGamesDropDown = true;
+        this.showSettingsDropDown = false;
+        event.stopPropagation();
+      }
+    }
+
+    if (type == "settings"){
+      if(!this.showSettingsDropDown){
+        this.showSettingsDropDown = true;
+        this.showGamesDropDown = false;
+        event.stopPropagation();
+      }
+    }
+  }
+
+  hideDropDown(type) {
+    if (type == "games"){
+      this.showGamesDropDown = false;
+    }
+
+    if (type == "settings"){
+      this.showSettingsDropDown = false;
+    }
+  }
+
+
+}
