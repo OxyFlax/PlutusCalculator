96959aa605f31b482a4aab94f18a7cd9cbfb5161
Gilles Thewissen <gilles.thewissen@gmail.com>
2020-07-30 15:11:47 +0200

    addition of basic google analytics tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+
+//declare gives Angular app access to ga function
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -7,7 +10,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(router: Router) {
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+        gtag('config', 'UA-174027920-1', {'page_path': event.urlAfterRedirects});
+      }
+    })
+
+    
     // redirects if sessionstorage contains a redirect entry, this is done to give support to github.io
     if (sessionStorage.getItem("redirect")) {
       router.navigateByUrl(sessionStorage.getItem("redirect"));
