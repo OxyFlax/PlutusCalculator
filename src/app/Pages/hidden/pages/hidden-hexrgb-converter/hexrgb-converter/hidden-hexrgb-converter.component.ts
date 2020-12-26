2c7b0972c7ae063437a97f5528a6f511c2aef883
GillesT <gilles.thewissen@gmail.com>
2020-12-26 17:07:51 +0100

    added shade & tint chart to rgb&hex converter

--- src/app/Pages/hidden/pages/hidden-hexrgb-converter/hexrgb-converter/hidden-hexrgb-converter.component.ts
+++ src/app/Pages/hidden/pages/hidden-hexrgb-converter/hexrgb-converter/hidden-hexrgb-converter.component.ts
@@ -7,7 +7,7 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./hidden-hexrgb-converter.component.css']
 })
 export class HiddenHexRGBConverterComponent implements OnInit, OnDestroy {
-  inputIsHex: boolean = true;
+  inputSetToHex: boolean = true;
 
   hexInput: string = "";
 
@@ -19,6 +19,9 @@ export class HiddenHexRGBConverterComponent implements OnInit, OnDestroy {
   rgbOutput: string = "";
 
   color: string = "";
+
+  colorShades: any[] = new Array(11);
+  colorTints: any[] = new Array(11);
 
   constructor(private titleService: Title, private metaService: Meta) { }
 
@@ -37,48 +40,96 @@ export class HiddenHexRGBConverterComponent implements OnInit, OnDestroy {
   }
 
   swapConversion() {
-    if (this.inputIsHex) {
-      this.inputIsHex = false;
+    if (this.inputSetToHex) {
+      this.inputSetToHex = false;
     } else {
-      this.inputIsHex = true;
+      this.inputSetToHex = true;
     }
     this.clearInput();
   }
 
   convert() {
-    if (this.inputIsHex) {
-      //convert hex to rgb
-      this.rgbOutput = this.hexToRGB(this.hexInput);
+    if (this.inputSetToHex) {
+      //convert hex to rgb, if it returns a value format the output and generate shades. Otherwise show null (prevents errors where red green & blue do not exist)
+      var rgbValues = this.hexToRGB(this.hexInput);
+      if(rgbValues) {
+        this.rgbOutput = "rgb(" + rgbValues.red + ", " + rgbValues.green + ", " + rgbValues.blue + ")";
+        this.createTintsAndShades(this.hexInput);
+      } else {
+        this.rgbOutput = null;
+        this.clearShadesAndTintsOutput();
+      }
       this.color = this.rgbOutput;
     } else {
-      // ensure the rgb input isn't below 0
-      this.rgbRedInput = this.rgbRedInput < 0 ? 0 : this.rgbRedInput;
-      this.rgbGreenInput = this.rgbGreenInput < 0 ? 0 : this.rgbGreenInput;
-      this.rgbBlueInput = this.rgbBlueInput < 0 ? 0 : this.rgbBlueInput;
-
-      // ensure the rgb input isn't above 255
-      this.rgbRedInput = this.rgbRedInput > 255 ? 255 : this.rgbRedInput;
-      this.rgbGreenInput = this.rgbGreenInput > 255 ? 255 : this.rgbGreenInput;
-      this.rgbBlueInput = this.rgbBlueInput > 255 ? 255 : this.rgbBlueInput;
-
-      //convert rgb to hex
-      this.hexOutput = this.RGBToHex(this.rgbRedInput, this.rgbGreenInput, this.rgbBlueInput);
+      //convert rgb to hex, format the output and generate shades
+      this.hexOutput = "#" + this.rgbToHex(this.rgbRedInput, this.rgbGreenInput, this.rgbBlueInput);
+      this.createTintsAndShades(this.hexOutput);
       this.color = this.hexOutput;
     }
   }
 
+  createTintsAndShades(hexColor) {
+    // convert the hexcolor to rgb to allow the calculations to work
+    var rgbColor = this.hexToRGB(hexColor);
+
+    // calculate an array of shades from the input color
+    this.colorShades = this.calculateShades(rgbColor);
+
+    // calculate an array of tints from the input color
+    this.colorTints = this.calculateTints(rgbColor);
+  }
+
+  calculateShades(color) {
+    var shadeValues = [];
+
+    for (var i = 0; i < 10; i++) {
+      shadeValues[i] = "#" + this.rgbToHex(color.red * (1 - 0.1 * i), color.green * (1 - 0.1 * i), color.blue * (1 - 0.1 * i));
+    }
+
+    shadeValues.push("#000000");
+
+    return shadeValues;
+  }
+
+  calculateTints(color) {
+    var tintValues = [];
+
+    for (var i = 0; i < 10; i++) {
+      tintValues[i] = "#" + this.rgbToHex(color.red + (255 - color.red) * i * 0.1, color.green + (255 - color.green) * i * 0.1, color.blue + (255 - color.blue) * i * 0.1);
+    }
+
+    tintValues.push("#FFFFFF");
+
+    return tintValues;
+  }
+
   hexToRGB(hexInput) {
     var parsedInput = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexInput);
-    if(parsedInput) {
-      var output = "rgb(" + parseInt(parsedInput[1], 16) + ", " + parseInt(parsedInput[2], 16) + ", " + parseInt(parsedInput[3], 16) + ")";
-      return output;
+    if (parsedInput) {
+      return {
+        red: parseInt(parsedInput[1], 16),
+        green: parseInt(parsedInput[2], 16),
+        blue: parseInt(parsedInput[3], 16)
+      }
     }
     return null;
   }
 
+  rgbToHex(r, g, b) {
+    var returnValue: string = this.intToHex(r) + this.intToHex(g) + this.intToHex(b);
+    return returnValue.toUpperCase();
+  }
 
-  RGBToHex(r, g, b) {
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase();
+  intToHex(rgbint) {
+    return this.pad(Math.min(Math.max(Math.round(rgbint), 0), 255).toString(16), 2);
+  }
+
+  pad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {
+      str = '0' + str;
+    }
+    return str;
   }
 
   clearInput() {
@@ -92,5 +143,12 @@ export class HiddenHexRGBConverterComponent implements OnInit, OnDestroy {
     this.rgbOutput = "";
 
     this.color = "";
+
+    this.clearShadesAndTintsOutput();
+  }
+
+  clearShadesAndTintsOutput() {
+    this.colorShades = new Array(11);
+    this.colorTints = new Array(11);
   }
 }
